6b42c27fec7175cd8c53634d1f07a9f0027774c7
hasyrails <runteq.learning.hasy@gmail.com>
2020-06-07 08:52:18 +0900

    [test] task編集成功のテストコードを作成

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -22,4 +22,47 @@ RSpec.describe "Tasks", type: :system do
       # No route matches {:action=>"show", :controller=>"tasks", :id=>nil}, missing required keys: [:id]
     end
   end
+
+  describe '#edit' do
+    it 'can edit a new task' do
+      # ログインする ===
+      user = FactoryBot.create(:user)
+      login(user)
+      sleep 1
+      # ===
+
+      # タスクを新規作成 ===
+      visit new_task_path
+      
+      task = FactoryBot.build(:task)
+      fill_in 'Title', with: task.title
+      select task.status, from: 'Status'
+      click_button 'Create Task'
+      # =====
+      
+      # タスクを編集 ===
+      visit edit_task_path(task)
+      
+      fill_in 'Title', with: 'ピアノ'
+      select task.status, from: 'Status'
+      sleep 1
+      click_button 'Update Task'
+      
+      expect(page).to have_content 'Task was successfully updated.'
+      
+      expect(current_path).to eq task_path(task)
+      # ===
+    end
+  end
 end
+
+# ↓task = FactoryBot.create(:task) で作成したタスクを編集することとすると、user_idが一致しないユーザーがアクセスすることとなるため、アクセスできない。
+# taskファクトリにuser_idを指定したら可か？
+# ====
+# task = FactoryBot.create(:task)      
+# visit edit_task_path(task)
+# fill_in 'Title', with: 'ピアノ'
+# select task.status, from: 'Status'
+# sleep 1
+# click_button 'Update Task'    
+# ===
