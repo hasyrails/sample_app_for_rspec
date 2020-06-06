a1fba20169606e537a4fa4624aace0136e2ff7ff
hasyrails <runteq.learning.hasy@gmail.com>
2020-06-07 08:15:46 +0900

    [test] taskの新規登録成功のテストコードを作成
    新規登録後のパスの検証 expect(current_path).to ...でファクトリにidの指定がないためエラー
    →taskファクトリにidの指定を追加

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :task, class: Task do
+    id { 1 }
+    # idを指定しない状態だとexpect(current_path).to eq task_path(task)が通らないため追加
     title { 'ランニング' }
     content { 'A公園' }
     status { 0 }
@@ -8,16 +10,16 @@ FactoryBot.define do
     association :user,
       factory: :user,
       email: 'test1@example.com'
-    end
+  end
     
-    factory :re_task, class: Task do
-      title { 'ランニング' }
-      content { 'A公園' }
-      status { 0 }
-      deadline { '2020-6-20 7:00' }
-      
-      association :user,
-        factory: :user,
-        email: 'test2@example.com'
+  factory :re_task, class: Task do
+    title { 'ランニング' }
+    content { 'A公園' }
+    status { 0 }
+    deadline { '2020-6-20 7:00' }
+    
+    association :user,
+      factory: :user,
+      email: 'test2@example.com'
   end
 end
--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -1,9 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe "Tasks", type: :system do
-  before do
-    driven_by(:rack_test)
-  end
+  describe '#create' do
+    it 'can create a new task' do
+      # ログインする
+      user = FactoryBot.create(:user)
+      login(user)
+      sleep 1
 
-  pending "add some scenarios (or delete) #{__FILE__}"
+      visit new_task_path
+
+      task = FactoryBot.build(:task)
+      fill_in 'Title', with: task.title
+      select task.status, from: 'Status'
+      click_button 'Create Task'
+      
+      expect(page).to have_content 'Task was successfully created.'
+      
+      expect(current_path).to eq task_path(task)
+      # ActionController::UrlGenerationError:
+      # No route matches {:action=>"show", :controller=>"tasks", :id=>nil}, missing required keys: [:id]
+    end
+  end
 end
