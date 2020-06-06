943b5560195b1cace27502468ae2129bc5caf36c
hasyrails <runteq.learning.hasy@gmail.com>
2020-06-06 20:06:07 +0900

    [test] ユーザー登録成功時の検証テストコード作成

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -4,9 +4,27 @@ RSpec.describe "Users", type: :system do
   describe '#create' do
     context 'user registration' do
       it 'can be created' do
-        new_user = FactoryBot.create(:user)
+        new_user = FactoryBot.build(:user)
 
         visit new_user_path
+        sleep 3
+
+        fill_in 'Email', with: new_user.email
+        fill_in "Password", with: 'pwd'
+        # fill_in "Password", with: new_user.password では入力されない
+        # sorceryでは「password」はvirtual attribute(DBに存在しない)→ActiveRecordメソッドで呼び出せない。
+        fill_in 'Password confirmation', with: 'pwd'
+        # fill_in "Password confirmation", with: new_user.password_confirmation では入力されない
+        # sorceryでは「password_confirmation」はvirtual attribute(DBに存在しない)→ActiveRecordメソッドで呼び出せない。
+        
+        sleep 3
+
+        click_button 'SignUp'
+
+        sleep 3
+
+        expect(page).to have_content 'User was successfully created.'
+        expect(current_path).to eq login_path
       end
     end
   end
