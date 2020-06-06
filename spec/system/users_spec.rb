244363664a7eb90c3fab67009f7c9d6b5599c597
hasyrails <runteq.learning.hasy@gmail.com>
2020-06-06 19:06:41 +0900

    [test]テストケース作成

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -1,4 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe "Users", type: :system do
+  describe '#create' do
+    context 'user registration' do
+      it 'can be created' do
+        new_user = FactoryBot.create(:user)
+
+        visit new_user_path
+      end
+    end
+  end
 end
