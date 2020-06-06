0941380cd91af612c06d7e35803cf1216e3d51bc
hasyrails <runteq.learning.hasy@gmail.com>
2020-06-06 21:11:46 +0900

    [test]ユーザ編集成功のテストコードを作成
    require_loginあり→ログイン画面へ遷移/ログイン情報入力
    editing_userファクトリとedited_userファクトリを使用　→要修正(editing_userのみで可)

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe "Users", type: :system do
         new_user = FactoryBot.build(:user)
 
         visit new_user_path
-        sleep 3
+        sleep 1
 
         fill_in 'Email', with: new_user.email
         fill_in "Password", with: 'pwd'
@@ -17,14 +17,47 @@ RSpec.describe "Users", type: :system do
         # fill_in "Password confirmation", with: new_user.password_confirmation では入力されない
         # sorceryでは「password_confirmation」はvirtual attribute(DBに存在しない)→ActiveRecordメソッドで呼び出せない。
         
-        sleep 3
+        sleep 1
 
         click_button 'SignUp'
 
-        sleep 3
+        sleep 1
 
         expect(page).to have_content 'User was successfully created.'
         expect(current_path).to eq login_path
+      end
+    end
+  end
+  
+  describe '#edit' do
+    context 'editing user' do
+      it 'can be edited' do
+
+        editing_user = FactoryBot.create(:user)
+        edited_user = FactoryBot.build(:edited_user)
+
+        visit login_path
+        fill_in 'Email', with: editing_user.email
+        fill_in 'Password', with: 'pwd'
+        click_button 'Login'
+        sleep 3
+        visit edit_user_path(editing_user)
+        sleep 1
+
+        fill_in 'Email', with: edited_user.email
+        fill_in "Password", with: 'edited_pwd'
+        fill_in 'Password confirmation', with: 'edited_pwd'
+        
+        sleep 1
+
+        click_button 'Update'
+
+        sleep 1
+
+        expect(page).to have_content 'User was successfully updated.'
+        expect(current_path).to eq user_path(editing_user)
+
+        sleep 1
       end
     end
   end
