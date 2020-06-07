c3abf12417759745058c10efe68ac9ec9c4ca378
hasyrails <runteq.learning.hasy@gmail.com>
2020-06-07 09:47:46 +0900

    [test] task削除成功のテストコードを作成
    close #11

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -40,7 +40,7 @@ RSpec.describe "Tasks", type: :system do
       click_button 'Create Task'
       # =====
       
-      # タスクを編集 ===
+      # 新規作成したタスクを編集 ===
       visit edit_task_path(task)
       
       fill_in 'Title', with: 'ピアノ'
@@ -54,9 +54,44 @@ RSpec.describe "Tasks", type: :system do
       # ===
     end
   end
+  
+  describe '#destroy' do
+    it 'can be deleted' do
+      # ログインする ===
+      user = FactoryBot.create(:user)
+      login(user)
+      sleep 1
+      # ===
+    
+      # タスクを新規作成 ===
+      visit new_task_path
+      
+      task = FactoryBot.build(:task)
+      fill_in 'Title', with: task.title
+      select task.status, from: 'Status'
+      click_button 'Create Task'
+      # =====
+      
+      # 新規作成したタスクを削除 ===
+      visit root_path
+      
+      click_link 'Destroy'
+
+      expect {
+        page.accept_confirm "Are you sure?"
+        expect(page).to have_content "Task was successfully destroyed."
+      }.to change { Task.count }.by(-1)
+
+      expect(page).to have_content 'Task was successfully destroyed.'
+      
+      expect(current_path).to eq tasks_path
+      # ===
+    end
+  end
 end
 
-# ↓task = FactoryBot.create(:task) で作成したタスクを編集することとすると、user_idが一致しないユーザーがアクセスすることとなるため、アクセスできない。
+# ※task編集成功のテストコードに関して
+# task = FactoryBot.create(:task) で作成したタスクを編集することとすると、user_idが一致しないユーザーがアクセスすることとなるため、アクセスできない。
 # taskファクトリにuser_idを指定したら可か？
 # ====
 # task = FactoryBot.create(:task)      
