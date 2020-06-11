a64bdb2649c9f496b80194cc5c9e60798df3d25b
hasyrails <runteq.learning.hasy@gmail.com>
2020-06-12 07:58:43 +0900

    spec/system/user_sessions_spec.rbをリファクタリング
    共通の前処理をbefore-doで記述
    テストパス確認済

--- spec/system/user_sessions_spec.rb
+++ spec/system/user_sessions_spec.rb
@@ -1,58 +1,62 @@
 require 'rails_helper'
 
 RSpec.describe 'UserSessions', type: :system do
-  describe 'ログイン前' do
-    context 'フォームの入力値が正常' do
-      it 'ログイン処理が成功する' do
-        login_user = FactoryBot.create(:user)
-        
+  describe 'ユーザーのログイン/ログアウト' do 
+    let(:user) { create(:user) }
+    describe 'ログイン前' do
+      before do
+        # let!(:user) { create(:user) }
         visit login_path
         sleep 1
-        
-        login(login_user)
-        sleep 1
-        
-        # post '/login', params: { user: FactoryBot.attributes_for(:user) }
-        # post :create, params: { user: FactoryBot.attributes_for(:user) }
-        # No match routesエラーが起こる
-        
-        # follow_redirect!
-        # not a redirectエラー
-        
-        expect(page).to have_content 'Login successful'
-        expect(current_path).to eq root_path
       end
-    end
+      context 'フォームの入力値が正常' do
+        it 'ログイン処理が成功する' do
+          
+          # post '/login', params: { user: FactoryBot.attributes_for(:user) }
+          # post :create, params: { user: FactoryBot.attributes_for(:user) }
+          # No match routesエラーが起こる
+          
+          # follow_redirect!
+          # not a redirectエラー
+          
+          login(user)
+          sleep 1
+          expect(page).to have_content 'Login successful'
+          expect(current_path).to eq root_path
+        end
+      end
 
-    context 'フォームが未入力' do
-      it 'ログイン処理が失敗する' do
-        user = FactoryBot.create(:user)
-        
-        visit login_path
-        sleep 1
-        
-        fill_in 'Email', with: ''
-        fill_in 'Password', with: 'pwd'
-        click_button 'Login'
-        expect(page).to have_content 'Login failed'
-        expect(current_path).to eq login_path
+      context 'フォームが未入力' do
+        it 'ログイン処理が失敗する' do
+          fill_in 'email', with: ''
+          sleep 3
+          fill_in 'Password', with: 'pwd'
+          click_button 'Login'
+          expect(page).to have_content 'Login failed'
+          expect(current_path).to eq login_path
+        end
       end
     end
-  end
-  
-  describe 'ログイン後' do
-    context 'ログアウトボタンをクリック' do
-      it 'ログアウト処理が成功する' do 
-        user = FactoryBot.create(:user)
+    
+    describe 'ログイン後' do
+      # before do
+        # user = FactoryBot.create(:user)
         
+        # login(user)
+      # end
+      before do
         login(user)
-        
-        click_link 'Logout'
-        sleep 1
+      end
+      context 'ログアウトボタンをクリック' do
+        it 'ログアウト処理が成功する' do 
+          
+          click_on 'Logout'
+          sleep 1
 
-        expect(page).to have_content 'Logged out'
-        expect(current_path).to eq root_path
-        sleep 1
+          expect(page).to have_content 'Logged out'
+          expect(current_path).to eq root_path
+          sleep 1
+        end
       end
     end
   end
