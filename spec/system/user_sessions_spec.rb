355e93620f9d3ab31a03e206096894a0f1a573da
hasyrails <runteq.learning.hasy@gmail.com>
2020-06-12 20:02:22 +0900

    [test] users_spec.rb user_sessions_spec.rbテストコード修正完了
    userファクトリのパスワードとテストコード内の入力パスワードに不整合あり、修正済

--- spec/system/user_sessions_spec.rb
+++ spec/system/user_sessions_spec.rb
@@ -5,20 +5,11 @@ RSpec.describe 'UserSessions', type: :system do
     let(:user) { create(:user) }
     describe 'ログイン前' do
       before do
-        # let!(:user) { create(:user) }
         visit login_path
         sleep 1
       end
       context 'フォームの入力値が正常' do
         it 'ログイン処理が成功する' do
-          
-          # post '/login', params: { user: FactoryBot.attributes_for(:user) }
-          # post :create, params: { user: FactoryBot.attributes_for(:user) }
-          # No match routesエラーが起こる
-          
-          # follow_redirect!
-          # not a redirectエラー
-          
           login(user)
           sleep 1
           expect(page).to have_content 'Login successful'
@@ -39,11 +30,6 @@ RSpec.describe 'UserSessions', type: :system do
     end
     
     describe 'ログイン後' do
-      # before do
-        # user = FactoryBot.create(:user)
-        
-        # login(user)
-      # end
       before do
         login(user)
       end
--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -1,138 +1,118 @@
 require 'rails_helper'
 
-RSpec.describe 'Users', type: :system do
+RSpec.describe 'Users', type: :system do  
   describe 'ログイン前' do
+    let(:user) { build(:user) }
+    let(:other_user) { build(:user)}
     describe 'ユーザー新規登録' do
-      context 'フォームの入力値が正常' do
+    before do
+      visit new_user_path
+      sleep 1
+    end
+      context 'フォームの入力値が正常' do     
         it 'ユーザーの新規作成が成功する' do
-          user = build(:user)
-
-          visit new_user_path
-          sleep 1
-
           fill_in 'Email', with: user.email
           fill_in "Password", with: 'password'
-          # fill_in "Password", with: new_user.password では入力されない
-          # sorceryでは「password」はvirtual attribute(DBに存在しない)→ActiveRecordメソッドで呼び出せない。
           fill_in 'Password confirmation', with: 'password'
-          # fill_in "Password confirmation", with: new_user.password_confirmation では入力されない
-          # sorceryでは「password_confirmation」はvirtual attribute(DBに存在しない)→ActiveRecordメソッドで呼び出せない。
+          sleep 1
           
-          sleep 1
-
           click_button 'SignUp'
-
           sleep 1
-
+          
           expect(page).to have_content 'User was successfully created.'
           expect(current_path).to eq login_path
+          sleep 1
         end
       end
-
+      
       context 'メールアドレスが未入力' do
         it 'ユーザーの新規作成が失敗する' do
-          user = build(:user)
-
-          visit new_user_path
-          sleep 1
-        
           fill_in 'Password', with: 'password'
           fill_in 'Password confirmation', with: 'password'
           click_button 'SignUp'
           sleep 1
-
+          
           expect(page).to have_content "Email can't be blank"
           expect(current_path).to eq users_path
-        
           expect(User.all.size).to eq 0
+          sleep 1
         end
       end
-
+      
       context '登録済のメールアドレスを使用' do
         it 'ユーザーの新規作成が失敗する' do
-          user = create(:user)
-          re_user = build(:user)
-        
-          visit new_user_path
-          sleep 1
-        
-          fill_in 'Email', with: re_user.email
-          fill_in 'Password', with: 'pwd'
-          fill_in 'Password confirmation', with: 'pwd'
+          fill_in 'Email', with: user.email
+          fill_in 'Password', with: 'password'
+          fill_in 'Password confirmation', with: 'password'
           click_button 'SignUp'
           sleep 1
-        
+          
+          click_on 'SignUp'
+          sleep 1
+
+          fill_in 'Email', with: user.email
+          fill_in 'Password', with: 'password'
+          fill_in 'Password confirmation', with: 'password'
+          click_button 'SignUp'
+          sleep 1
+          
           expect(page).to have_content "Email has already been taken"
           expect(current_path).to eq users_path
-        
           expect(User.all.size).to eq 1
+          sleep 1
         end
       end
     end
-  end
- 
+    
     describe 'マイページ' do
+      let(:user) { create(:user) }
+      before do
+        visit user_path(user)
+        sleep 1
+      end
       context 'ログインしていない状態' do
         it 'マイページへのアクセスが失敗する' do
-          user = create(:user)
-
-          visit user_path(user)
-          sleep 1
-        
           expect(page).to have_content 'Login required'
           expect(current_path).to eq login_path
         end
       end
     end
- 
+  end
+  
   describe 'ログイン後' do
+  let(:user) { create(:user) }
+  let(:other_user) { create(:user) }
+  let(:task) { build(:task) }
+  before do
+    login(user)
+  end
+  
     describe 'ユーザー編集' do
+    before do
+      visit user_path(user)
+      click_on 'Edit'
+      sleep 1
+    end
+
       context 'フォームの入力値が正常' do
         it 'ユーザーの編集が成功する' do
-          
-          editing_user = create(:user)
-          # editing_user = FactoryBot.build(:user)ではDB上にレコード登録されていないのでログイン情報として使用不可
-
-          # 編集するにはbefore_action :require_loginを通る必要がある
-
-          login(editing_user)
-
-          sleep 3
-        
-          visit edit_user_path(editing_user)
-          sleep 1
-
-          fill_in 'Email', with: editing_user.email
+          fill_in 'Email', with: user.email
           fill_in "Password", with: 'edited_pwd'
           fill_in 'Password confirmation', with: 'edited_pwd'
-        
           sleep 1
-
+          
           click_button 'Update'
-
           sleep 1
 
           expect(page).to have_content 'User was successfully updated.'
 
-          expect(current_path).to eq user_path(editing_user)
-          # 編集前のユーザーファクトリをediting_user, 編集後のユーザーファクトリをedited_userと区別すると、idが異なってしまうため、詳細画面のパスエラー（ActionController::UrlGenerationError:No route matches）となる。区別する必要なし。
-
+          expect(current_path).to eq user_path(user)
           sleep 1
-
         end
       end
-    end
-  end
 
       context 'メールアドレスが未入力' do
         it 'ユーザーの編集が失敗する' do 
-          user = create(:user)
-        
-          login(user)
-          
-          visit edit_user_path(user)
-          sleep 1
-          
           fill_in 'Email', with: ''
           # 指定しない状態だと、予めフォームに入力されているメールアドレスがあるので空となっていない。
           
@@ -148,30 +128,20 @@ RSpec.describe 'Users', type: :system do
 
       context '登録済のメールアドレスを使用' do
         it 'ユーザーの編集が失敗する' do
-          user = create(:user)
-          re_user = create(:re_user, email: 'hoge@example.com')
-          
-          login(re_user)
-          
-          visit edit_user_path(re_user)
-          sleep 1
-          
-          fill_in 'Email', with: user.email
-          fill_in 'Password', with: 'edited_pwd'
-          fill_in 'Password confirmation', with: 'edited_pwd'
+          fill_in 'Email', with: other_user.email
+          fill_in 'Password', with: 'password'
+          fill_in 'Password confirmation', with: 'password'
           click_button 'Update'
           sleep 1
           
           expect(page).to have_content "Email has already been taken"
-          expect(current_path).to eq user_path(re_user)
+          expect(current_path).to eq user_path(user)
         end
       end
+    end
 
       context '他ユーザーの編集ページにアクセス' do
         it '編集ページへのアクセスが失敗する' do
-          user = create(:user)
-          other_user = create(:other_user)
-      
           login(other_user)
           sleep 1
 
@@ -182,27 +152,32 @@ RSpec.describe 'Users', type: :system do
           expect(current_path).to eq user_path(other_user)
         end
       end
- 
+    
+      
     describe 'マイページ' do
+    before do
+      visit new_task_path
+    end
+        
       context 'タスクを作成' do
         it '新規作成したタスクが表示される' do
-          # ログインする
-          user = create(:user) # id=1のuser
-          login(user)
-          sleep 1
-
-          # login_userが作成したタスクを用意
-          task_by_login_user = create(:task, user_id: 1)
-    
+          
+          fill_in 'Title', with: task.title
+          fill_in 'Content', with: task.content
+          select task.status, from: 'task_status'
+          fill_in 'Deadline', with: task.deadline
+          click_on 'Create Task'
+          
           visit tasks_path
           sleep 1
-
+          
           # ログインしたユーザーが作成したタスクが表示されているか
-          expect(page).to have_content task_by_login_user.title
-          expect(page).to have_content task_by_login_user.content
-          expect(page).to have_content task_by_login_user.status
-          expect(page).to have_content task_by_login_user.deadline.strftime('%Y/%-m/%-d %-H:%-M')
+          expect(page).to have_content task.title
+          expect(page).to have_content task.content
+          expect(page).to have_content task.status
+          expect(page).to have_content task.deadline.strftime('%Y/%-m/%-d %-H:%-M')
         end
       end
     end
   end
+end
